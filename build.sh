c4fbc86243dadd184a70c9f210d8ac550f5349a7
cppassembly01-dev <cppassembly01@gmail.com>
2025-12-27 16:27:05 -0300

    Setup configs and build scripts

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PATH=~/oneplus/prebuilts/clang-r536225/bin:~/oneplus/prebuilts/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-linux-gnu/bin:$PATH
+export ARCH=arm64
+export SUBARCH=arm64
+export LLVM=1
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export KBUILD_BUILD_USER=akronnos
+export KBUILD_BUILD_HOST=archlinux
+rm -rf out
+make O=out lemonades_defconfig
+make O=out -j$(nproc --all)
