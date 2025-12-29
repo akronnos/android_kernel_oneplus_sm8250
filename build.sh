04d6a46cf28e7d845da890b3f5862b0d84f290cf
Akron <124622631+akronnos@users.noreply.github.com>
2025-12-29 21:41:26 +0000

    Update build.sh to include mkbootimg commands

--- build.sh
+++ build.sh
@@ -11,3 +11,24 @@ export KBUILD_BUILD_HOST=archlinux
 rm -rf out
 make O=out lemonades_defconfig
 make O=out -j$(nproc --all)
+
+mkbootimg/unpack_bootimg.py --boot_img \
+mkbootimg/boot.img --format mkbootimg \
+--out mkbootimg/out
+
+cp out/arch/arm64/boot/Image \
+mkbootimg/out/kernel
+
+mkbootimg/mkbootimg.py \
+--header_version 2 --os_version 15.0.0 \
+--os_patch_level 2025-10 \
+--kernel mkbootimg/out/kernel \
+--ramdisk mkbootimg/out/ramdisk \
+--dtb mkbootimg/out/dtb --pagesize 0x00001000 \
+--base 0x00000000 --kernel_offset 0x00008000 \
+--ramdisk_offset 0x01000000 \
+--second_offset 0x00000000 \
+--tags_offset 0x00000100 \
+--dtb_offset 0x0000000001f00000 --board '' \
+--cmdline 'androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 cgroup.memory=nokmem,nosocket loop.max_part=7 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 reboot=panic_warm service_locator.enable=1 swiotlb=2048' \
+--output mkbootimg/out/boot.img
